f485ba393431e7d62f7370df299d3ff97c047af2
SyamSai45 <syamsai455@gmail.com>
2026-04-19 13:12:18 +0530

    fix: vite config for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
+  base: "/",   // 🔥 IMPORTANT
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
